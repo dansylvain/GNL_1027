05d8c8ed57819b6bac26dba4ec370c7152e2a9de
dansylvain <dansylvain84@gmail.comgit config --global user.email dansylvain84@gmail.com>
2023-10-27 18:29:24 +0200

    initial commit

---
+++ get_next_line.c
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 09:57:00 by dsylvain          #+#    #+#             */
+/*   Updated: 2023/10/27 18:27:28 by dan              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+
+	i = 0;
+	while (i < n)
+		((unsigned char *)s)[i++] = (unsigned char)c;
+	return (s);
+}
+
+int	initialise_variables(t_Data **data)
+{
+	if (!*data)
+	{
+		*data = (t_Data *)malloc(sizeof(t_Data));
+		(*data)->buff_nl = NULL;
+		(*data)->buffer = NULL;
+	}
+	if (!(*data)->buff_nl)
+	{
+		(*data)->buff_nl = (char *)malloc(1);
+		(*data)->buff_nl[0] = '\0';
+	}
+	if (!(*data)->buffer)
+	{
+		(*data)->buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		ft_memset((*data)->buffer, '\0', BUFFER_SIZE + 1);
+		(*data)->buff_nl[0] = '\0';
+	}
+	if (!*data || !(*data)->buff_nl)
+		return (0);
+	(*data)->bytes_read = 1;
+	return (1);
+}
+
+char	*build_next_line(t_Data **data, char *tmp, int bytes_read)
+{
+	char	*next_line;
+
+	if (ft_strchr((*data)->buff_nl, '\n'))
+	{
+		next_line = ft_substr((*data)->buff_nl, 0, tmp - (*data)->buff_nl + 1);
+		ft_strlcpy((*data)->buff_nl, tmp + 1, ft_strlen(tmp));
+		return (next_line);
+	}
+	else if (bytes_read == 0 && *(*data)->buff_nl)
+	{
+		next_line = ft_substr((*data)->buff_nl, 0, ft_strlen((*data)->buff_nl));
+		*(*data)->buff_nl = '\0';
+		return (next_line);
+	}
+	return (NULL);
+}
+
+void	*free_all(t_Data **data)
+{
+	free((*data)->buff_nl);
+	free((*data)->buffer);
+	(*data)->buff_nl = NULL;
+	(*data)->buffer = NULL;
+	free(*data);
+	*data = NULL;
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_Data	*data = NULL;
+	char			*next_line;
+	char			*tmp;
+
+	initialise_variables(&data);
+	if (data->bytes_read == 0)
+		return (NULL);
+	while (!(ft_strchr(data->buff_nl, '\n')) && data->bytes_read)
+	{
+		data->bytes_read = read(fd, data->buffer, BUFFER_SIZE);
+		if (data->bytes_read == -1)
+			return (free_all(&data));
+		data->buffer[data->bytes_read] = '\0';
+		tmp = data->buff_nl;
+		data->buff_nl = ft_strjoin(data->buff_nl, data->buffer);
+		free(tmp);
+	}
+	tmp = ft_strchr(data->buff_nl, '\n');
+	next_line = build_next_line(&data, tmp, data->bytes_read);
+	if (next_line)
+		return (next_line);
+	free_all(&data);
+	return (NULL);
+}
+// GERER RETOUR DE READ = -1
+// int main(void)
+// {
+// 	int	fd;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 		return(0);
+// }
---
+++ get_next_line_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 10:00:32 by dan               #+#    #+#             */
+/*   Updated: 2023/10/27 16:30:39 by dan              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	if ((start + len) > ft_strlen(s))
+		substr = (char *)malloc(sizeof(char) * (ft_strlen(s) - start + 1));
+	else
+		substr = (char *)malloc(sizeof(char) * len + 1);
+	if (substr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)(s));
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(s));
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (size == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	i;
+
+	res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < ft_strlen(s2))
+	{
+		res[i + ft_strlen(s1)] = s2[i];
+		i++;
+	}
+	res[i + ft_strlen(s1)] = '\0';
+	return (res);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
