582cd74aa30a6032a0e8d8e26361afbba49f2643
dansylvain <dansylvain84@gmail.comgit config --global user.email dansylvain84@gmail.com>
2023-10-29 11:56:59 +0100

    cleaning modifs made

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 04:58:59 by dsylvain          #+#    #+#             */
-/*   Updated: 2023/10/29 10:11:59 by dan              ###   ########.fr       */
+/*   Updated: 2023/10/29 11:49:55 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # endif
 
 # include <unistd.h>
-# include <sys/stat.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
@@ -33,12 +32,9 @@ typedef struct t_Data
 
 int		initialise_variables(t_Data **data);
 char	*build_next_line(t_Data **data, char *tmp, int bytes_read);
-void	*delete_data(t_Data **data);
+void	delete_data(t_Data **data);
 char	*get_next_line(int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-char	*ft_strndup(const char *s, size_t n);		
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 04:58:59 by dsylvain          #+#    #+#             */
-/*   Updated: 2023/10/29 11:22:26 by dan              ###   ########.fr       */
+/*   Updated: 2023/10/29 11:52:20 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # endif
 
 # include <unistd.h>
-# include <sys/stat.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
@@ -37,9 +36,6 @@ char	*build_next_line(t_Data **data, char *tmp, int bytes_read);
 void	*delete_data(t_Data **data, int fd);
 char	*get_next_line(int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-char	*ft_strndup(const char *s, size_t n);		
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
