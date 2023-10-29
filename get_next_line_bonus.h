7a631057c659e4f3aa11562b79c2c9dc85ee6573
dansylvain <dansylvain84@gmail.comgit config --global user.email dansylvain84@gmail.com>
2023-10-29 15:14:43 +0100

    change made - no cleaning

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,14 +6,14 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 04:58:59 by dsylvain          #+#    #+#             */
-/*   Updated: 2023/10/29 14:04:35 by dan              ###   ########.fr       */
+/*   Updated: 2023/10/29 14:32:12 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 50
 # endif
 
 # include <unistd.h>
